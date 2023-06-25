4d2ee0161499e1b53c3801ae3cc21d5d844cebf1
Zafarbek <zzaaffaarr333@gmail.com>
2023-06-26 02:25:39 +0500

    Second

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -19,7 +19,7 @@ class _LoginPageState extends State<LoginPage> {
           Container(
             width: w,
             height: h*0.3,
-            decoration: const BoxDecoration(
+            decoration: BoxDecoration(
               image: DecorationImage(
                 image: AssetImage(
                   "img/loginimg.png"
@@ -34,7 +34,7 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(
+                Text(
                   "Hello",
                   style: TextStyle(
                     fontSize: 70,
@@ -48,16 +48,127 @@ class _LoginPageState extends State<LoginPage> {
                       color: Colors.grey[500]
                   ),
                 ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30)
-                    )
+                SizedBox(height: h*0.04,),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10,
+                            spreadRadius: 7,
+                            offset: Offset(1, 1),
+                            color: Colors.grey.withOpacity(0.2)
+                        )
+                      ]
                   ),
-                )
+                  child: TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide(
+                                color: Colors.white,
+                                width: 1.0
+                            )
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide(
+                                color: Colors.white,
+                                width: 1.0
+                            )
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30)
+                        )
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10,
+                            spreadRadius: 7,
+                            offset: Offset(1, 1),
+                            color: Colors.grey.withOpacity(0.2)
+                        )
+                      ]
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide(
+                                color: Colors.white,
+                                width: 1.0
+                            )
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide(
+                                color: Colors.white,
+                                width: 1.0
+                            )
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30)
+                        )
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Row(
+                  children: [
+                    Expanded(child: Container(),),
+                    Text(
+                      "Forgot your Password?",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.grey[500]
+                      ),
+                    )
+                  ],
+                ),
+
               ],
             ),
-          )
+          ),
+          SizedBox(height: h*0.05,),
+          Container(
+            width: w*0.5,
+            height: h*0.08,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+                image: DecorationImage(
+                    image: AssetImage(
+                        "img/loginbtn.png"
+                    ),
+                    fit: BoxFit.cover
+                ),
+            ),
+            child:Center(
+              child: Text(
+                "Sign in",
+                style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: w*0.08,),
+          RichText(text: TextSpan(
+            text: "Don't have an account?",
+            style: TextStyle(
+              color: Colors.grey[500],
+              fontSize: 20
+            )
+          ))
         ],
       ),
     );
